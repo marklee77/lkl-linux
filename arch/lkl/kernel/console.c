a9d5a406957f35d4c680a9fe49d80d558706ddde
Hajime Tazaki <thehajime@gmail.com>
2015-12-14 22:41:55 +0900

    lkl: add stdin oper using rump hypercall
    Signed-off-by: Hajime Tazaki <thehajime@gmail.com>

--- arch/lkl/kernel/console.c
+++ arch/lkl/kernel/console.c
@@ -18,13 +18,6 @@ static void console_write(struct console *con, const char *str, unsigned len)
 	}
 }
 
-static ssize_t file_write(struct file *fp, const char __user *s,
-			  size_t n, loff_t *off)
-{
-	console_write(NULL, s, n);
-	return n;
-}
-
 #ifdef CONFIG_LKL_EARLY_CONSOLE
 static struct console lkl_boot_console = {
 	.name	= "lkl_boot_console",
@@ -41,10 +34,51 @@ int __init lkl_boot_console_init(void)
 early_initcall(lkl_boot_console_init);
 #endif
 
-/* only support stdout */
+/* only support writer */
+static struct console lkl_console = {
+	.name	= "lkl_console",
+	.write	= console_write,
+	.flags	= CON_PRINTBUFFER,
+	.index	= -1,
+};
+
+
+int __init lkl_console_init(void)
+{
+	register_console(&lkl_console);
+	return 0;
+}
+core_initcall(lkl_console_init);
+
+static ssize_t file_write(struct file *fp, const char __user *s,
+			  size_t n, loff_t *off)
+{
+	console_write(NULL, s, n);
+	return n;
+}
+
+static ssize_t file_read(struct file *file, char __user *buf, size_t size,
+			 loff_t *ppos)
+{
+	struct rumpuser_iovec iov;
+	ssize_t ret;
+	int err;
+
+	iov.iov_base = buf;
+	iov.iov_len = size;
+
+	err = rumpuser_iovread(0, &iov, 1, 0, &ret);
+	if (err == 0) {
+		return ret;
+	}
+
+	return -err;
+}
+
 static struct file_operations lkl_stdio_fops = {
 	.owner		= THIS_MODULE,
 	.write =	file_write,
+	.read =		file_read,
 };
 
 static int __init lkl_stdio_init(void)
@@ -79,19 +113,3 @@ static int __init lkl_memdev_init(void)
 	return 0;
 }
 device_initcall(lkl_memdev_init);
-
-static struct console lkl_console = {
-	.name	= "lkl_console",
-	.write	= console_write,
-	.flags	= CON_PRINTBUFFER,
-	.index	= -1,
-};
-
-
-int __init lkl_console_init(void)
-{
-	register_console(&lkl_console);
-	return 0;
-}
-core_initcall(lkl_console_init);
-
